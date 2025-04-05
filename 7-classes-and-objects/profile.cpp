650149e6af762d1aadf658afb9565007f0f60fd5
sbhatia57 <bhatia7999@gmail.com>
2025-04-05 11:50:16 -0400

    The Object Of Your Affection
    I completed the project.

--- 7-classes-and-objects/profile.cpp
+++ 7-classes-and-objects/profile.cpp
@@ -1,37 +1,38 @@
 #include <iostream>
-
 #include "profile.hpp"
 
 Profile::Profile(std::string new_name, int new_age, std::string new_city, std::string new_country, std::string new_pronouns)
-  : name(new_name), age(new_age), city(new_city), country(new_country), pronouns(new_pronouns) {
-
-  if (new_age >= 18) {
+{
+    name = new_name;
     age = new_age;
-  } else {
-    age = 0;
-  }
-
+    city = new_city;
+    country = new_country;
+    pronouns = new_pronouns;
 }
 
-std::string Profile::view_profile() {
+std::string Profile::view_profile()
+{
+    std::string profileInfo = "Name: " + name + "\n";
+    profileInfo += "age: " + std::to_string(age);
+    profileInfo += "\ncity: " + city;
+    profileInfo += "\ncountry: " + country;
+    profileInfo += "\npronouns: " + pronouns;
+    profileInfo += "\nhobbies: ";
 
-  std::string bio = "Name: " + name;
-  bio += "\nAge: " + std::to_string(age);
-  bio += "\nPronouns: " + pronouns;
-  std::string hobby_string = "Hobbies:\n";
+    // Loop through users's hobbies included in their profile
+    for (int hobbyIndex = 0; hobbyIndex < hobbies.size(); hobbyIndex++)
+    {
+        // Deals with edge case of the last item
+        if ((hobbyIndex + 1) < hobbies.size())
+            profileInfo += hobbies[hobbyIndex] + ", ";
+        else
+            profileInfo += hobbies[hobbyIndex];
+    }
 
-  for (std::string hobby : hobbies) {
-
-    hobby_string += " - " + hobby + "\n";
-
-  }
-
-  return bio + "\n" + hobby_string;
-
+    return profileInfo;
 }
 
-void Profile::add_hobby(std::string new_hobby) {
-
-  hobbies.push_back(new_hobby);
-
+void Profile::add_hobby(std::string new_hobby)
+{
+    hobbies.push_back(new_hobby);
 }
--- 7-classes-and-objects/profile.hpp
+++ 7-classes-and-objects/profile.hpp
@@ -1,17 +1,17 @@
 #include <vector>
 
-class Profile {
+class Profile
+{
 private:
-  std::string name;
-  int age;
-  std::string city;
-  std::string country;
-  std::string pronouns;
-  std::vector<std::string> hobbies;
-  
+    std::string name;
+    int age;
+    std::string city;
+    std::string country;
+    std::string pronouns;
+    std::vector<std::string> hobbies;
 public:
-  Profile(std::string new_name, int new_age, std::string new_city, std::string new_country, std::string new_pronouns = "they/them");
-  std::string view_profile();
-  void add_hobby(std::string new_hobby);
+    Profile(std::string new_name, int new_age, std::string new_city, std::string new_country, std::string new_pronouns = "they/them");
+    std::string view_profile();
+    void add_hobby(std::string new_hobby);
 
-};
+}; #pragma once
